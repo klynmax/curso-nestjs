ed047c730f383a4c6bdce6ca09b40b1c42c4c06e
Klynsman Lemos <klynsman.lemos@itemm.org.br>
2026-01-21 09:33:23 -0300

    Criando as conexão do módulo de produtos

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class ProductService {
-  constructor(private prisma: PrismaClient);
-  
+  constructor(private prisma: PrismaService) {}
+
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
