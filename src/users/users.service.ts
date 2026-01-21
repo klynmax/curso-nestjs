fbea2baa9ec6030a0c95263e9db293f67ca07cfa
Klynsman Lemos <klynsman.lemos@itemm.org.br>
2026-01-21 10:06:03 -0300

    Removendo objeto comentado

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,27 +5,6 @@ import { PrismaService } from "src/prisma.service";
 
 @Injectable()
 export class UserService {
-    // private users = [
-    //     {
-    //         id: 1,
-    //         name: 'Luiz Inácio',
-    //         email: 'fazol@email.com',
-    //         password: '123'
-    //     },
-    //     {
-    //         id: 2,
-    //         name: 'Flávio Caça Rato',
-    //         email: 'paidemagrao@email.com',
-    //         password: '123'
-    //     },
-    //     {
-    //         id: 3,
-    //         name: 'Eliza Sanches',
-    //         email: 'sanches_ousadah@email.com',
-    //         password: '123'
-    //     },
-    // ];
-
     constructor(private prisma: PrismaService) {}
 
     async findAll(id: number) {
