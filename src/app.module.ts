6ce1d8c83765e3dc20c88f8ff3f517cc761206e0
Klynsman Lemos <klynsman.lemos@itemm.org.br>
2026-01-21 09:27:44 -0300

    Criando um módulo através do comando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/users.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ProductModule],
   controllers: [],
   providers: [],
 })
